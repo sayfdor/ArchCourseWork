0d67b29dcf4e0ef1b05bd72bf612d73feaed9701
sayfdor <mrdesper.mi@gmail.com>
2023-11-16 01:08:26 +0300

    ver. 5

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,44 @@
 /* Paths */
+
 const srcPath = "src/"
 const distPath = "dist/"
 
 const path = {
     src: {
         html: srcPath + "templates/*.html",
-        css: srcPath + "static/css/*.css",
+        css: srcPath + "static/**/*.css",
         img: srcPath + "static/img/**/*.{jpg,png,svg}"
     },
 
     build: {
-      html: distPath
+        html: distPath,
+        css: distPath + "static/css/"
     },
 
     watch: {
         html: srcPath + "**/*.html",
-        css: srcPath + "static/**/*.css",
+        css: srcPath + "static/css/*.css",
         img: srcPath + "static/img/**/*.{jpg,png,svg}"
     }
 }
 
+
+ /* Tasks */
+
 gulp = require('gulp');
 rigger = require('gulp-rigger');
 
-    gulp.task('html', function() {
+    gulp.task('build', function() {
         gulp.src(path.watch.html)
             .pipe(rigger())
-            .pipe(gulp.dest(path.build.html));
+            .pipe(gulp.dest(path.build.html))
     });
+
+gulp = require('gulp');
+concatCss = require('gulp-concat');
+
+gulp.task('default', function () {
+    return gulp.src(path.watch.css)
+        .pipe(concatCss("styles/bundle.css"))
+        .pipe(gulp.dest('out/'));
+});
